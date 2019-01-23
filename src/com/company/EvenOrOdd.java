487da89caf4dbaa207752a881994ac246a186277
Muhammad Shah <m_hussain_shah@hotmail.com>
2019-01-23 17:56:17 -0500

    Added Classes

--- src/com/company/EvenOrOdd.java
+++ src/com/company/EvenOrOdd.java
@@ -1,16 +1,28 @@
 package com.company;
+
+import java.util.Scanner;
+
 /*
  * Take integer input from the user, output whether it's odd or even.
- * The letter grade is determined as follows:
- * A = 90–100%
- * B = 80–89%
- * C = 70–79%
- * D = 60–69%
- * F = 0–59%
- * Write a program that will print the letter grade based on the percentage entered.
  */
 public class EvenOrOdd {
     public static void main(String[] args) {
+        promptUser();
+    }
+    public static void promptUser(){
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Enter number : ");
+        int num1 = keyboard.nextInt();
+
+        check(num1);
+    }
+
+    public static void check(int num1){
+        if(num1%2==0){
+            System.out.println(num1 +" is an Even Number");
+        } else {
+            System.out.println(num1 + " is an Odd Number");
+        }
     }
 }
