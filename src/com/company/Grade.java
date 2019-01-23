389372547bfa45bee412fe56f0e3661ee74b05bc
Muhammad Shah <m_hussain_shah@hotmail.com>
2019-01-23 17:56:05 -0500

    Added Classes

---
+++ src/com/company/Grade.java
@@ -0,0 +1,15 @@
+package com.company;
+/*
+ * The letter grade is determined as follows:
+ * A = 90–100%
+ * B = 80–89%
+ * C = 70–79%
+ * D = 60–69%
+ * F = 0–59%
+ * Write a program that will print the letter grade based on the percentage entered.
+ */
+public class Grade {
+    public static void main(String[] args) {
+        
+    }
+}
